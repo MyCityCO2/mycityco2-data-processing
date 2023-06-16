a32b35b4f75ee370d291bf3529c853719c2bdb35
Remy Zulauff <rzu@open-net.ch>
2023-06-16 11:45:51 +0200

    feat(const.py): add pydantic settings class to handle environment variables and add new settings for logging, notifications, and database configuration feat(.env.example): add example environment variable for MCO2DP_ENV_PASSWORD feat(.flake8): add E501 and E800 to the list of ignored flake8 errors to allow lines up to 160 characters long and to ignore some flake8-bugbear warnings
    feat(const.py): add pydantic settings class to handle environment variables and add new settings for logging, notifications, and database configuration
    feat(.env.example): add example environment variable for MCO2DP_ENV_PASSWORD
    feat(.flake8): add E501 and E800 to the list of ignored flake8 errors to allow lines up to 160 characters long and to ignore some flake8-bugbear warnings

---
+++ mycityco2_data_process/const.py
@@ -0,0 +1,47 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    class Config:
+        env_prefix = "MCO2DP_"
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+    LOGORU_FORMAT: str = "<green>{time:YYYY-MM-DD at HH:mm:ss}</green> <level>{level}</level> - {message}"
+    LOGURU_LEVEL: str = "DEBUG"
+
+    NTFY_TOGGLE: bool = False
+    NTFY_LEVEL: str = "ERROR"
+    NTFY_TOPICS: list[str] = ["opennetabo"]
+    NTFY_TITLE: str = "MyCityCO2 Importer"
+    NTFY_SERVER: str = "https://ntfy.sh/"
+
+    ENV_DELETE_DB_TOGGLE: bool = True
+    ENV_URL: str = "https://tests-odoo-16c.odooapps.ch"
+    ENV_TEMPLATE_DB: str = "bch-mycity-co2-temlate-test"
+    # ENV_DB: str = "bch-foron-departement"
+    # ENV_URL: str = "http://172.25.0.3:8069"
+    # ENV_TEMPLATE_DB: str = 'template'
+    ENV_DB: str = "temp-departement"
+    ENV_USERNAME: str = "__system__"
+    ENV_PASSWORD: str
+
+    CITY_CHUNK_SIZE: int = 1000
+    ACCOUNT_CHUNK_SIZE: int = 6000
+    ACCOUNT_ASSET_CHUNK_SIZE: int = 2000
+
+    CARBON_FILE: str = "static/fr/fr_mapping_coa_exiobase.csv"
+
+    ACCOUNT_ASSET_TOGGLE: bool = True
+    ACCOUNT_ASSET_FILE: str = "static/fr/fr_mapping_immo_exiobase.csv"
+
+    YEAR: list = list(range(2010, 2022))
+    # YEAR: list = [2021]
+    DEFAULT_ACCOUNT_TYPE: str = "off_balance"
+
+    # SKIPPED_CITY: str = ['Allinges', 'Amancy']
+    SKIPPED_CITY: list[str] = []
+
+    SQL_PORT = 666
+    SQL_LOCAL = False
+    # SQL_PORT = 667
